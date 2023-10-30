f10f1d1ee887444d964aa333d86a114cdc3594aa
Naman-12911 <namangarg820@gmail.com>
2023-10-30 18:27:48 +0530

    add procfile

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import Register,Login,userData,update_profile,PasswordResetAPIView
+from .views import Register,Login,userData,update_profile
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
@@ -7,12 +7,4 @@ urlpatterns = [
      path('login/', Login.as_view(),name="login"),
      path('profile/',userData.as_view(),name="profile"),
      path('update-profile/',update_profile.as_view(),name="update_profile"),
-
-    path('password_reset/', PasswordResetView.as_view(html_email_template_name='registration/password_reset_email.html'), name='account_password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-     path('password_reset/', PasswordResetAPIView.as_view(), name='password_reset')
-    
 ]
