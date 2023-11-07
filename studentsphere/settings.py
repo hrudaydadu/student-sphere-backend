33b89096113675456beb992a5714f2b482024fc0
Naman-12911 <namangarg820@gmail.com>
2023-11-07 12:41:25 +0530

    csrf token

--- studentsphere/settings.py
+++ studentsphere/settings.py
@@ -210,6 +210,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "https://studentsphere-b734aba5fe3c.herokuapp.com",
 ]
+CSRF_TRUSTED_ORIGINS =[
+    'http://localhost:3000',
+    "http://127.0.0.1:8000",
+    "https://studentsphere-b734aba5fe3c.herokuapp.com",
+]
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
