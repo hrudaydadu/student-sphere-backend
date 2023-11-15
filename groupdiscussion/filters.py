003de688b85a09a8b741a3584017b868fae71d7f
Naman-12911 <namangarg820@gmail.com>
2023-11-15 20:03:41 +0530

    add cname

--- groupdiscussion/filters.py
+++ groupdiscussion/filters.py
@@ -5,7 +5,7 @@ class SearchCollageFilters(django_filters.FilterSet):
    class Meta:
         model = SearchCollage
         fields = {
-            'name': ['exact'],
+            'name': ['icontains'],
             
         }
 
